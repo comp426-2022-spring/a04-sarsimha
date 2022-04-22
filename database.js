0fc0efbf747eacd94620cd7760eabb615ddd7c03
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-04-22 00:09:16 -0400

    syntax

--- database.js
+++ database.js
@@ -8,29 +8,16 @@ const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and n
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
-if (row == undefined) {
+
+if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
     const sqlInit = `
-        CREATE TABLE acesslog (
-            id INTEGER PRIMARY KEY,
-            remoteaddr TEXT,
-            remoteuser TEXT,
-            time INTEGER, 
-            method TEXT,
-            url TEXT,
-            protocol TEXT,
-            httpversion TEXT,
-            secure TEXT,
-            status INTEGER,
-            referer TEXT,
-            useragent TEXT
-    );`;
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT,  protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT);`;
     db.exec(sqlInit);
-    console.log('Database has been initialized.');
-} 
-else {
+    console.log('Your database has been initialized.');
+} else {
 // Since the database already exists, echo that to the console.
     console.log('Database exists.')
 }
 // Export all of the above as a module so that we can use it elsewhere.
-module.exports = db	
+module.exports = db
