4deacadfaf5fac0a43a6090f23ca174b151c4f90
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-04-22 00:03:53 -0400

    errors

--- database.js
+++ database.js
@@ -4,35 +4,29 @@ const Database = require('better-sqlite3');
 
 // Connect to a database or create one if it doesn't exist yet.
 const db = new Database('log.db');
-
-const stmt = db.prepare(`
-    SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`);
+const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and name='accesslog';`)
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
-if (row === undefined) {
-    console.log('Your database appears to be empty. I will initialize it now.');
-    const sqlInit = `
-        CREATE TABLE acesslog (
-            id INTEGER PRIMARY KEY,
-            remoteaddr TEXT,
-            remoteuser TEXT,
-            time INTEGER, 
-            method TEXT,
-            url TEXT,
-            protocol TEXT,
-            httpversion TEXT,
-            secure TEXT,
-            status INTEGER,
-            referer TEXT,
-            useragent TEXT
-    );`;
-    db.exec(sqlInit);
-    console.log('Database has been initialized.');
-} 
-else {
-// Since the database already exists, echo that to the console.
-    console.log('Database exists.')
+if (row == undefined){
+    console.log('Log database appears to be empty. Create log database...')
+    const sq1Init = ` CREATE TABLE accesslog ( 
+        id INTEGER PRIMARY KEY,
+        remoteaddr VARCHAR,
+        remoteuser VARCHAR,
+        time VARCHAR,
+        method VARCHAR,
+        url VARCHAR,
+        protocol VARCHAR,
+        httpversion NUMERIC,
+        secure VARCHAR,
+        status INTEGER,
+        referer VARCHAR,
+        useragent VARCHAR 
+        );`
+    db.exec(sq1Init);
+} else {
+    console.log('Log Databse exists')
 }
-// Export all of the above as a module so that we can use it elsewhere.
-module.exports = db
+
+module.exports = db;
