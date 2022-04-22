14ad8c1b6cfc89cac650d9aff8b7a8eec645f0e7
sarsimha <69766312+sarsimha@users.noreply.github.com>
2022-04-22 00:13:53 -0400

    fixing format

--- database.js
+++ database.js
@@ -8,11 +8,21 @@ const stmt = db.prepare(`SELECT name FROM sqlite_master WHERE type='table' and n
 // Define row using `get()` from better-sqlite3
 let row = stmt.get();
 // Check if there is a table. If row is undefined then no table exists.
-
 if (row === undefined) {
     console.log('Your database appears to be empty. I will initialize it now.');
     const sqlInit = `
-        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, remoteaddr TEXT, remoteuser TEXT, time TEXT, method TEXT, url TEXT,  protocol TEXT, httpversion TEXT, secure TEXT, status TEXT, referer TEXT, useragent TEXT);`;
+        CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, 
+            remoteaddr TEXT, 
+            remoteuser TEXT, 
+            time TEXT, 
+            method TEXT, 
+            url TEXT,  
+            protocol TEXT,
+            httpversion TEXT,
+            secure TEXT, 
+            status TEXT, 
+            referer TEXT, 
+            useragent TEXT);`;
     db.exec(sqlInit);
     console.log('Database has been initialized.');
 } else {
